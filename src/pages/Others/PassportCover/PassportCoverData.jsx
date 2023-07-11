ab85cbe06ed6e3c770dc3116311d1c3466019222
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-07-11 12:31:35 +0600

    ok

--- src/pages/Others/PassportCover/PassportCoverData.jsx
+++ src/pages/Others/PassportCover/PassportCoverData.jsx
@@ -1,7 +1,7 @@
 const passportCoverData = [
     {
       id: 1,
-      picture: "passport-cover1.jpg",
+      picture: "https://wildwovenleatherproducts.com/wp-content/uploads/2020/12/Passport-Holder-PH3-Yale-Blue-Inside-Price.jpg",
       name: "Leather Passport Cover",
       price: "$19.99",
       description: "The Leather Passport Cover is a stylish and functional accessory for your travel needs. It is made from high-quality leather and features multiple card slots, a passport pocket, and a secure closure.",
@@ -10,7 +10,7 @@ const passportCoverData = [
     },
     {
       id: 2,
-      picture: "passport-cover2.jpg",
+      picture: "https://m.media-amazon.com/images/I/81cJiJSwAUL._AC_UY1100_.jpg",
       name: "Minimalist Passport Holder",
       price: "$14.99",
       description: "The Minimalist Passport Holder is a sleek and compact option for keeping your passport safe. It is crafted from durable materials and features a slim design with a clear ID window and a card slot.",
@@ -19,7 +19,7 @@ const passportCoverData = [
     },
     {
       id: 3,
-      picture: "passport-cover3.jpg",
+      picture: "https://robishopbd.com/storage/3459/IMG_20230130_175447_415-removebg-preview.png",
       name: "RFID Blocking Passport Wallet",
       price: "$24.99",
       description: "The RFID Blocking Passport Wallet provides extra security for your passport and cards. It is made from RFID-blocking material to protect against electronic theft and features multiple card slots and a passport pocket.",
@@ -28,7 +28,7 @@ const passportCoverData = [
     },
     {
       id: 4,
-      picture: "passport-cover4.jpg",
+      picture: "https://m.media-amazon.com/images/I/71tIEPOPkOL._AC_UY1100_.jpg",
       name: "Leather Travel Document Holder",
       price: "$29.99",
       description: "The Leather Travel Document Holder is designed to keep all your travel essentials organized. It is crafted from genuine leather and features multiple card slots, a passport pocket, and additional compartments for tickets and documents.",
@@ -37,7 +37,7 @@ const passportCoverData = [
     },
     {
       id: 5,
-      picture: "passport-cover5.jpg",
+      picture: "https://m.media-amazon.com/images/I/41ERZp+RlRS.jpg",
       name: "Personalized Passport Cover",
       price: "$22.99",
       description: "The Personalized Passport Cover adds a personal touch to your travel accessories. It is customizable with your initials and is made from high-quality materials, offering protection and style for your passport.",
@@ -46,7 +46,7 @@ const passportCoverData = [
     },
     {
       id: 6,
-      picture: "passport-cover6.jpg",
+      picture: "https://www.covershop.com.bd/wp-content/uploads/2022/08/charm-rfid-vintage-multi-function-pu-leather-passport-covers-holder-wallet-case-3.webp",
       name: "Vintage Leather Passport Holder",
       price: "$34.99",
       description: "The Vintage Leather Passport Holder is a classic accessory that exudes elegance. It is made from premium leather with a distressed finish, featuring a passport pocket, card slots, and a pen holder.",
@@ -55,7 +55,7 @@ const passportCoverData = [
     },
     {
       id: 7,
-      picture: "passport-cover7.jpg",
+      picture: "https://vivagifts.in/wp-content/uploads/2022/04/Travelling-Combo-Of-Passport-Cover-Luggage-Tag-And-Eyewear-Case-2.jpeg",
       name: "Passport Cover with Luggage Tag",
       price: "$27.99",
       description: "The Passport Cover with Luggage Tag is a matching set that adds a touch of style to your travel gear. The passport cover features multiple card slots, a passport pocket, and the luggage tag ensures easy identification of your bags.",
@@ -64,7 +64,7 @@ const passportCoverData = [
     },
     {
       id: 8,
-      picture: "passport-cover8.jpg",
+      picture: "https://www.cuyana.com/on/demandware.static/-/Sites-master-catalog-cuyana/default/dw3bd5f7c2/DesignDetail_HO22_SlimPassportCase_DD_2325.jpg",
       name: "Slim Passport Sleeve",
       price: "$17.99",
       description: "The Slim Passport Sleeve is a minimalist and lightweight option for protecting your passport. It is made from durable materials and offers a snug fit for your passport, ensuring it stays secure during your travels.",
@@ -73,7 +73,7 @@ const passportCoverData = [
     },
     {
       id: 9,
-      picture: "passport-cover9.jpg",
+      picture: "https://tudiaproducts.com/cdn/shop/products/22ea386539e82791b5a46fc468f34340_1024x1024.png?v=1543602242",
       name: "Passport Cover with RFID Blocking",
       price: "$26.99",
       description: "The Passport Cover with RFID Blocking technology provides an extra layer of security for your personal information. It is designed to prevent unauthorized scanning of your passport and cards, keeping your identity safe.",
@@ -82,7 +82,7 @@ const passportCoverData = [
     },
     {
       id: 10,
-      picture: "passport-cover10.jpg",
+      picture: "https://gadgetsin.com/uploads/2021/06/handmade_personalized_leather_passport_holder_with_zipper_closure_1.jpg",
       name: "Passport Holder with Zipper",
       price: "$21.99",
       description: "The Passport Holder with Zipper offers added security and organization for your travel essentials. It features a zippered closure to keep your passport and cards safe, along with additional compartments for tickets and cash.",
